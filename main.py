4b512f35dc9847ae07121b117e52530576881c84
ruri <m5@ruri.live>
2026-04-27 21:32:58 +0800

    5514

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from app import app, socketio
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=47263, debug=True, use_reloader=False)
+    socketio.run(app, host='0.0.0.0', port=4444, debug=True, use_reloader=False)
